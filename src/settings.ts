c4ee0b008363f077294a76b1d7e5c30b10905f15
Petesrs0nW <peterweiss0@icloud.com>
2021-01-17 22:41:52 +0100

    changedName

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'shell-commander';
+export const PLATFORM_NAME = 'Shell-Commander';
 
 /**
  * This must match the name of your plugin as defined the package.json
